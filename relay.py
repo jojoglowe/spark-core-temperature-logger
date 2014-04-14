7bfa64e31870daecfc0a6f8cfa47ef99dc969e49
Ryan Palmer <rypalmer@gmail.com>
2014-04-14 18:49:55 +0000

    Trying to catch errors a bit more reliably

--- relay.py
+++ relay.py
@@ -8,10 +8,10 @@ spark_core_id = os.environ['SPARK_CORE_ID']
 spark_core_token = os.environ['SPARK_CORE_TOKEN']
 
 librato_api = librato.connect(librato_user, librato_auth)
-spark_messages = SSEClient("https://api.spark.io/v1/devices/%s/events/?access_token=%s" % (spark_core_id, spark_core_token))
 
 while True:
   try:
+    spark_messages = SSEClient("https://api.spark.io/v1/devices/%s/events/?access_token=%s" % (spark_core_id, spark_core_token))
     for msg in spark_messages:
       if msg.event == "temperature1":
         data = json.loads(msg.data);
@@ -20,10 +20,9 @@ while True:
   except socket.gaierror:
     print "Connection error. Trying again in a minute."
     time.sleep(60)
-  except requests.exceptions.HTTPError:
-    print "504 Server error. Trying again in a minute."
+  except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
+    print "Server request error. Trying again in a minute."
     time.sleep(60)
-    spark_messages = SSEClient("https://api.spark.io/v1/devices/%s/events/?access_token=%s" % (spark_core_id, spark_core_token))
   except librato.exceptions.BadRequest:
     print "Librato error. Trying again in a minute."
     time.sleep(60)
